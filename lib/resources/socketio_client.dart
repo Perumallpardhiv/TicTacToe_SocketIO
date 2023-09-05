a873c7d5c5f76d549e187bc6090740283ddf1762
Perumallpardhiv <lit2021001@iiitl.ac.in>
2023-09-06 02:31:28 +0530

    server hosted and updated link

--- lib/resources/socketio_client.dart
+++ lib/resources/socketio_client.dart
@@ -5,7 +5,7 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io('http://172.70.106.12:5000', <String, dynamic>{
+    socket = IO.io('https://server-ethj.onrender.com', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
