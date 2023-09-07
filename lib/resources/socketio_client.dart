c5394d1cd61d3085cf108526043e4762f7bc3c6d
Perumallpardhiv <lit2021001@iiitl.ac.in>
2023-09-07 21:44:59 +0530

    small change

--- lib/resources/socketio_client.dart
+++ lib/resources/socketio_client.dart
@@ -5,7 +5,7 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io('https://server-ethj.onrender.com', <String, dynamic>{
+    socket = IO.io('https://server-ethj.onrender.com/', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
